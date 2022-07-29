a984478c20de07d19e595a4d6ffb9b1cc30faa59
Rajinder Kumar <100748165+Rajinderking@users.noreply.github.com>
2022-07-29 17:50:45 +0530

    Add files via upload

---
+++ partials/_connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$database = "social_connections";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+/*if($conn){
+	echo "connected successfully";
+}
+else{
+	echo "error".mysqli_error($conn);
+}*/
+?>
---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+
+<div style="min-height:13rem;">
+
+
+
+</div>
+<div class ="container-fluid bg-dark text-light bottom" style = "margin-top:6%;bottom:          0px;width :100%; " >
+<p class = "text-center mb-0" > copyright Social Connections 2022 | All right reserved.</p>
+
+
+
+</div>
